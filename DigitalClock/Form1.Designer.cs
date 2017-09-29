1881da61a14d7155fc04577ea9496c368bbac134
C. Herrera <pro.cah@gmail.com>
2017-09-28 22:29:18 -0500

    Initial file upload
    This is first version, runs along with other tool

---
+++ DigitalClock/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace DigitalClock
+{
+    partial class frm_clock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing)
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_clock));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbl_time = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbl_time
+            // 
+            this.lbl_time.AutoSize = true;
+            this.lbl_time.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lbl_time.Font = new System.Drawing.Font("Lucida Console", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_time.Location = new System.Drawing.Point(33, 59);
+            this.lbl_time.Name = "lbl_time";
+            this.lbl_time.Size = new System.Drawing.Size(468, 65);
+            this.lbl_time.TabIndex = 0;
+            this.lbl_time.Text = "--:--:-- --";
+            // 
+            // frm_clock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 185);
+            this.Controls.Add(this.lbl_time);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frm_clock";
+            this.Text = "Digital Clock";
+            this.Load += new System.EventHandler(this.frm_clock_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbl_time;
+    }
+}
+
---
+++ DigitalClock/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigitalClock
+{
+    public partial class frm_clock : Form
+    {
+        public frm_clock ()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick (object sender, EventArgs e)
+        {
+            lbl_time.Text = DateTime.Now.ToString("hh:mm:ss tt");
+        }
+
+        private void frm_clock_Load (object sender, EventArgs e)
+        {
+            this.Text += " v" + Application.ProductVersion;
+        }
+    }
+}
